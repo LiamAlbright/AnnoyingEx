a388059d3c9ebe881330f0bf25a6af078e63a535
LiamAlbright <albrightliam56@gmail.com>
2020-05-24 19:58:39 -0700

    notific works

---
+++ app/src/main/java/com/example/annoyingex/AskStuffActivity.kt
@@ -0,0 +1,17 @@
+package com.example.annoyingex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_stuff.*
+
+class AskStuffActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stuff)
+
+        val someDataFromNotification = intent.getStringExtra("hello")
+
+        tvInfo.text = someDataFromNotification
+    }
+}
